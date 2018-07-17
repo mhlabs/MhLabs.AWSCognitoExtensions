f0885bfb4e192df71f0851161f50fd8cb6350656
Lars <lars@mathem.se>
2018-07-17 14:40:54 +0200

    first commit

---
+++ ClaimsPrincipalExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace MhLabs.AWSCognitoExtensions
+{
+    public static class ClaimsPrincipalExtension    
+    {
+        public static string UserId(this ClaimsPrincipal user) {            
+            if (user == null || !user.Identity.IsAuthenticated) return null;            
+            var userIdClaim = user.Claims.FirstOrDefault(c => c.Type == "cognito:username") ?? user.Claims.FirstOrDefault(c => c.Type == "username");
+            return userIdClaim?.Value;
+        }        
+    }
+}
