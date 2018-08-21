8ed5f5d501864b9910b88a8023aa4bd1f139f3b8
Lars <lars@mathem.se>
2018-08-21 14:51:39 +0200

    adding more claims

--- ClaimsPrincipalExtension.cs
+++ ClaimsPrincipalExtension.cs
@@ -8,8 +8,20 @@ namespace MhLabs.AWSCognitoExtensions
     {
         public static string UserId(this ClaimsPrincipal user) {            
             if (user == null || !user.Identity.IsAuthenticated) return null;            
-            var userIdClaim = user.Claims.FirstOrDefault(c => c.Type == "cognito:username") ?? user.Claims.FirstOrDefault(c => c.Type == "username");
-            return userIdClaim?.Value;
+            var claim = user.Claims.FirstOrDefault(c => c.Type == "cognito:username") ?? user.Claims.FirstOrDefault(c => c.Type == "username");
+            return claim?.Value;
+        }        
+
+        public static string Audience(this ClaimsPrincipal user) {            
+            if (user == null || !user.Identity.IsAuthenticated) return null;            
+            var claim = user.Claims.FirstOrDefault(c => c.Type == "aud") ?? user.Claims.FirstOrDefault(c => c.Type == "ClientId");
+            return claim?.Value;
+        }        
+        
+        public static string GetClaim(this ClaimsPrincipal user, string type) {            
+            if (user == null || !user.Identity.IsAuthenticated) return null;            
+            var claim = user.Claims.FirstOrDefault(c => c.Type == type);
+            return claim?.Value;
         }        
     }
 }
