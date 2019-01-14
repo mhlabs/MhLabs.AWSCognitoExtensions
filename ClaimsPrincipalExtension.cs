7b5012631a7993228f961dbc902081b64315c298
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2019-01-14 14:39:32 +0100

    Added support for local cognito user fallback from environment variables

--- ClaimsPrincipalExtension.cs
+++ ClaimsPrincipalExtension.cs
@@ -4,24 +4,41 @@ using System.Security.Claims;
 
 namespace MhLabs.AWSCognitoExtensions
 {
-    public static class ClaimsPrincipalExtension    
+    public static class ClaimsPrincipalExtension
     {
-        public static string UserId(this ClaimsPrincipal user) {            
-            if (user == null || !user.Identity.IsAuthenticated) return null;            
+        public static string UserId(this ClaimsPrincipal user)
+        {
+            if (user == null) return null;
+            if (user.Claims != null && !user.Claims.Any() && !user.Identity.IsAuthenticated) return LocalFallback();
+            if (!user.Identity.IsAuthenticated) return null;
+
             var claim = user.Claims.FirstOrDefault(c => c.Type == "cognito:username") ?? user.Claims.FirstOrDefault(c => c.Type == "username");
             return claim?.Value;
-        }        
+        }
 
-        public static string Audience(this ClaimsPrincipal user) {            
-            if (user == null || !user.Identity.IsAuthenticated) return null;            
+        private static string LocalFallback()
+        {
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            {
+                var userId = Environment.GetEnvironmentVariable("COGNITO_FALLBACK_USER_ID");
+                return userId;
+            }
+
+            return null;
+        }
+
+        public static string Audience(this ClaimsPrincipal user)
+        {
+            if (user == null || !user.Identity.IsAuthenticated) return null;
             var claim = user.Claims.FirstOrDefault(c => c.Type == "aud") ?? user.Claims.FirstOrDefault(c => c.Type == "client_id");
             return claim?.Value;
-        }        
+        }
 
-        public static string GetClaim(this ClaimsPrincipal user, string type) {            
-            if (user == null || !user.Identity.IsAuthenticated) return null;            
+        public static string GetClaim(this ClaimsPrincipal user, string type)
+        {
+            if (user == null || !user.Identity.IsAuthenticated) return null;
             var claim = user.Claims.FirstOrDefault(c => c.Type == type);
             return claim?.Value;
-        }        
+        }
     }
 }
